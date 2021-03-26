a91651892d04ad6816639ac9a84087f9ed899885
PINKPHANTOMS <55456019+PINKPHANTOMS@users.noreply.github.com>
2021-03-26 16:29:43 -0700

    lk

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@
 // Edited by SacrificeProductions
 
 var cols, rows;
-var scl = 50;
+var scl = 20;
 var w = 1400;
 var h = 1000;
 
@@ -15,8 +15,16 @@ var flying = 0;
 
 var terrain = [];
 
+function preload(){
+  sound = loadSound('sounds/fm.mp3');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  let cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+  cnv.mouseClicked(togglePlay);
+  fft = new p5.FFT();
+  sound.amp(0.9);
+
   cols = w / scl;
   rows = h / scl;
 
@@ -32,22 +40,27 @@ function draw() {
 
   push();
   translate(0, -100, 0);
+  if(sound.isPlaying()){
   rotateX(millis() / 1000);
   rotateY(millis() / 1000);
+}
   ambientLight(100); // white light
-  ambientMaterial(200, 200, 200); // gray material
+  ambientMaterial(0, 0, 0); // gray material
   box(100);
   pop();
 
-  flying -= 0.05;
+  let waveform = fft.waveform();
+
+
+  flying -= 0.1;
   var yoff = flying;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
-      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-      xoff += 0.1;
+      terrain[x][y] = map(noise(xoff*waveform[x], yoff*waveform[y]), -1, 1, -100, 100);
+      xoff += 0.2;
     }
-    yoff += 0.1;
+    yoff += 0.2;
   }
 
   background(0);
@@ -62,5 +75,14 @@ function draw() {
       vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
     }
     endShape();
+  }
+}
+
+
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
   }
 }
